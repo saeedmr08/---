dbe6920fcd97441d0db388d50ea829d38e69ffc3
saeed saeed <sss@macbook-pro-alkhas-b-saeed.local>
2021-11-07 17:35:32 +0300

    Initial Commit

---
+++ ---/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ---
+//
+//  Created by sss on 02/04/1443 AH.
+//  Copyright © 1443 sss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
